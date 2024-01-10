2607d8bb658abc09f2f1cf570fe81ffb27936869
Chloé T. <chloe.thomas14@gmail.com>
2024-01-10 16:47:23 +0100

    mouseover activé

--- content-script.js
+++ content-script.js
@@ -1,18 +1,10 @@
 console.log("hello world")
 
-const p = document.querySelector("h1")
-console.log("p = ", p)
+const giveMeTheFont = (event) => {
+    const elementClicked = event.target
+    const styleofElementCLicked = getComputedStyle(elementClicked)
+    const font = styleofElementCLicked.fontFamily
+    console.log(font)
+}
 
-const style = getComputedStyle(p)
-console.log("getComputedStyle = ", style)
-
-const color = style.color
-console.log("color = ", color)
-
-const font = style.fontFamily
-console.log(font)
-
-
-// for (let i=0; i<p.length; i++) {
-//     p[i].style.color = "green"
-// }
+document.addEventListener("mouseover", giveMeTheFont)
