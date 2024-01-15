a91edbce6996823dc20d23607072d598ce39a8a5
Emedim93 <emedim.araneda@gmail.com>
2024-01-15 12:29:17 +0100

    récup

--- content-script.js
+++ content-script.js
@@ -1,18 +1,18 @@
 const giveMeTheFont = (event) => {
-    const elementOver = event.target
+    const elementOver = event.target // l'evenement se déroule lors du clic
+    elementOver.className += " tooltip-container"
     const styleofElementOver = getComputedStyle(elementOver)
     const font = styleofElementOver.fontFamily
-    
-    const body = document.querySelector("body")
+   /* const body = document.querySelector("body")*/
     const tooltip = document.createElement('div') // ont créer un élément enfant qui est la div avec laquelle otn manipulera le CSS
     tooltip.className = "tooltip" 
     tooltip.textContent = font // ici, ont appel la variable font qui contient toute les polices
-    body.appendChild(tooltip)
+    elementOver.appendChild(tooltip)
     console.log(tooltip)
 
     //elementOver.datatoggle = "tooltip"
     //elementOver.title = font
 }
 
-document.addEventListener("mouseover", giveMeTheFont)
+document.addEventListener("click", giveMeTheFont)
 
