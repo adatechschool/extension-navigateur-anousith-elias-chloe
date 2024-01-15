4308fe0ab5ddefc33117bcdc2b93ad5eb08a45ca
Chloé T. <chloe.thomas14@gmail.com>
2024-01-15 12:22:39 +0100

    màj

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const giveMeTheFont = (event) => {
     const elementClicked = event.target
     const styleofElementCLicked = getComputedStyle(elementClicked)
     const font = styleofElementCLicked.fontFamily
-
     elementClicked.datatoggle = "tooltip"
     elementClicked.title = font
 }
 
 document.addEventListener("mouseover", giveMeTheFont)
 
-
+// "content_scripts": [
+//     {
+//     "js": ["script.js"],
+//     "matches": ["https://*/*","http://*/*"]
+//     }
+// ]
